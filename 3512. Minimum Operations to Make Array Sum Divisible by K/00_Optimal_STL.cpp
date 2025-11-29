6715a864bfbd24e542265d3ff2b62fda6cf8cf37
Aryan Tomar <aryantomar5000@gmail.com>
2025-11-29 18:28:05 +0530

    3512. Minimum Operations to Make Array Sum Divisible by K
    --> Optimal STL

---
+++ 3512. Minimum Operations to Make Array Sum Divisible by K/00_Optimal_STL.cpp
@@ -0,0 +1,23 @@
+// LeetCode (3512. Minimum Operations to Make Array Sum Divisible by K): 
+// https://leetcode.com/problems/minimum-operations-to-make-array-sum-divisible-by-k 
+
+// Tags: [EASY], [ARRAY], [MATH] 
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+class Solution {
+public:
+    int minOperations(vector<int>& nums, int k) {
+        int sum = accumulate(nums.begin(), nums.end(), 0);
+        return sum % k;
+    }
+};
+
+
+// T.C. = O(n) 
+// S.C. = O(1) 
+
+// n = nums.size() 
