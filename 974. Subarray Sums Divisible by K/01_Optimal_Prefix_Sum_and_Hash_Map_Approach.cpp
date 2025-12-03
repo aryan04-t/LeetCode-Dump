f1e2224fb41bc68f3cdbaf6d18962f4ada04e8d9
Aryan Tomar <aryantomar5000@gmail.com>
2025-12-04 04:30:40 +0530

    974. Subarray Sums Divisible by K
    --> Optimal Prefix Sum and Hash Map Approach

---
+++ 974. Subarray Sums Divisible by K/01_Optimal_Prefix_Sum_and_Hash_Map_Approach.cpp
@@ -0,0 +1,41 @@
+// LeetCode (974. Subarray Sums Divisible by K): 
+// https://leetcode.com/problems/subarray-sums-divisible-by-k 
+
+
+// TAGS: [RATING: 1675], [MEDIUM], [OPIMAL], [SUBARRAY], [PREFIX SUM], [MODULAR ARITHMETIC], [NEGATIVE REMAINDER], [HASH MAP] 
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+class Solution {
+public:
+    int subarraysDivByK(vector<int>& nums, int k) {
+        
+        int n = nums.size();
+
+        unordered_map<int, int> remainderCountMap;
+        remainderCountMap[0]++;
+
+        int totalSubArr = 0;
+        int runningSum = 0;
+
+        for (int i=0; i < n; i++) {
+            int val = nums[i];
+            
+            runningSum += val;
+            int remainder = runningSum % k;
+            if (remainder < 0) remainder += k;
+
+            totalSubArr += remainderCountMap[remainder];
+            remainderCountMap[remainder]++;
+        }
+
+        return totalSubArr;
+    }
+};
+
+
+// T.C. = O(n) 
+// S.C. = O(k) 
