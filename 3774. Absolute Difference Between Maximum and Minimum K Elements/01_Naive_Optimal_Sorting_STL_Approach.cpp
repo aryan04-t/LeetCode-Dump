6140f680be1a694f66be9d5ca2651d60e9ebd58b
Aryan Tomar <aryantomar5000@gmail.com>
2025-12-14 13:18:05 +0530

    3774. Absolute Difference Between Maximum and Minimum K Elements
    --> Naive Optimal Sorting STL Approach

---
+++ 3774. Absolute Difference Between Maximum and Minimum K Elements/01_Naive_Optimal_Sorting_STL_Approach.cpp
@@ -0,0 +1,52 @@
+// LeetCode (3774. Absolute Difference Between Maximum and Minimum K Elements) 
+// https://leetcode.com/problems/absolute-difference-between-maximum-and-minimum-k-elements 
+
+
+// TAGS: [RATING: N/A], [EASY], [NAIVE OPTIMAL], [STL], [ARRAY], [SORTING], [SUM] 
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+class Solution {
+public:
+    int absDifference(vector<int>& nums, int k) {
+        
+        sort(nums.begin(), nums.end());
+        
+        int kTemp = k;
+        int smallSum = accumulate(
+            nums.begin(),
+            nums.end(),
+            0,
+            [&kTemp](int acc, int num) {
+                if (kTemp > 0) {
+                    acc += num;
+                    kTemp--;
+                }
+                return acc;
+            }
+        );
+
+        kTemp = k;
+        int bigSum = accumulate(
+            nums.rbegin(),
+            nums.rend(),
+            0,
+            [&kTemp](int acc, int num) {
+                 if (kTemp > 0) {
+                    acc += num;
+                    kTemp--;
+                }
+                return acc;
+            }
+        );
+
+        return abs(bigSum - smallSum);
+    }
+};
+
+
+// T.C. = O(n*log(n)) + O(n) + O(n) = O(n*log(n)) 
+// S.C. = O(log(n)) 
