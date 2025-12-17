8061dbf19957563a0fb2147b8716de9422a19bce
Aryan Tomar <aryantomar5000@gmail.com>
2025-12-17 13:25:11 +0530

    Added [GUARD CLAUSES] Tag

--- 1930. Unique Length-3 Palindromic Subsequences/00_Naive_Approach.cpp
+++ 1930. Unique Length-3 Palindromic Subsequences/00_Naive_Approach.cpp
@@ -2,7 +2,7 @@
 // https://leetcode.com/problems/unique-length-3-palindromic-subsequences 
 
 
-// TAGS: [RATING: 1533], [MEDIUM], [NAIVE], [TLE ERROR], [STRING], [SUBSEQUENCE], [PALINDROME], [HASH SET] 
+// TAGS: [RATING: 1533], [MEDIUM], [NAIVE], [TLE ERROR], [STRING], [SUBSEQUENCE], [PALINDROME], [HASH SET], [GUARD CLAUSES] 
 
 
 #include<bits/stdc++.h>
