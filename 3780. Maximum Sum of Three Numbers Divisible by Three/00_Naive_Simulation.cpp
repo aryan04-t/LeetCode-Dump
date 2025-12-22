b10249de3b8994b1c8d65d5bdac5cab5b1a22a60
Aryan Tomar <aryantomar5000@gmail.com>
2025-12-22 17:41:56 +0530

    3780. Maximum Sum of Three Numbers Divisible by Three
    --> Naive Simulation Approach

---
+++ 3780. Maximum Sum of Three Numbers Divisible by Three/00_Naive_Simulation.cpp
@@ -0,0 +1,36 @@
+// LeetCode (3780. Maximum Sum of Three Numbers Divisible by Three): 
+// https://leetcode.com/problems/maximum-sum-of-three-numbers-divisible-by-three 
+
+
+// TAGS: [RATING: N/A], [MEDIUM], [NAIVE], [TLE ERROR], [ARRAY], [SIMULATION], [SUM], [MODULAR ARITHMETIC], [MAXIMIZING] 
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+class Solution {
+public:
+    int maximumSum(vector<int>& nums) {
+        
+        int n = nums.size();
+        int maxiSum = INT_MIN;
+
+        for (int i=0; i < n-2; i++) {
+            for (int j=i+1; j < n-1; j++) {
+                for (int k=j+1; k < n; k++) {
+                    int sum = nums[i] + nums[j] + nums[k];
+                    if (sum % 3 == 0) {
+                        maxiSum = max(maxiSum, sum);
+                    }
+                }
+            }
+        }
+
+        return maxiSum == INT_MIN ? 0 : maxiSum;
+    }
+};
+
+
+// T.C. = O(n^3) 
+// S.C. = O(1) 
